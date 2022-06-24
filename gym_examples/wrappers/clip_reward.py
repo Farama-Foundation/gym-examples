b5d391271c88f995368bcd8de6cc7c35db9aea05
Markus28 <montcyril@gmail.com>
2022-06-24 22:05:59 +0200

    Use self.observation_space instead of self._observation_space

--- gym_examples/wrappers/clip_reward.py
+++ gym_examples/wrappers/clip_reward.py
@@ -7,7 +7,7 @@ class ClipReward(gym.RewardWrapper):
         super().__init__(env)
         self.min_reward = min_reward
         self.max_reward = max_reward
-        self._reward_range = (min_reward, max_reward)
+        self.reward_range = (min_reward, max_reward)
 
     def reward(self, reward):
         return np.clip(reward, self.min_reward, self.max_reward)
