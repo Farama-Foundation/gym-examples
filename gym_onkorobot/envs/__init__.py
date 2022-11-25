474c9abec095f3e643348d8adcf5bdac7dda33c6
light5551 <sergey.glazunov.99@mail.ru>
2022-11-26 01:22:00 +0300

    change from gridworld to gym_onkorobot

---
+++ gym_onkorobot/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_onkorobot.envs.onkorobot import OnkoRobotEnv
--- gym_onkorobot/envs/onkorobot.py
+++ gym_onkorobot/envs/onkorobot.py
@@ -1,39 +1,27 @@
-import gym
-from gym import spaces
+import gymnasium as gym
+from gymnasium import spaces
 import pygame
 import numpy as np
+from gymnasium.spaces import Box, Discrete
+
+ACTION_SHAPE = (3, 1)
 
 
-class GridWorldEnv(gym.Env):
+class OnkoRobotEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=5):
-        self.size = size  # The size of the square grid
+    def __init__(self, render_mode=None,
+                 point_cloud=None):
         self.window_size = 512  # The size of the PyGame window
 
-        # Observations are dictionaries with the agent's and the target's location.
-        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
-        self.observation_space = spaces.Dict(
-            {
-                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-            }
-        )
+        # TODO
+        self.observation_space = spaces.Dict()
 
-        # We have 4 actions, corresponding to "right", "up", "left", "down", "right"
-        self.action_space = spaces.Discrete(4)
-
-        """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        I.e. 0 corresponds to "right", 1 to "up" etc.
-        """
-        self._action_to_direction = {
-            0: np.array([1, 0]),
-            1: np.array([0, 1]),
-            2: np.array([-1, 0]),
-            3: np.array([0, -1]),
-        }
+        # We have action [ [x, y, z], t ], where x, y, z - offset of current pos and t - time of dose
+        self.action_space = spaces.Tuple((
+            Box(-1, 1, shape=(3,)),
+            Discrete(1)
+        ))
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
@@ -49,9 +37,11 @@ class GridWorldEnv(gym.Env):
         self.clock = None
 
     def _get_obs(self):
+        # TODO: return point cloud
         return {"agent": self._agent_location, "target": self._target_location}
 
     def _get_info(self):
+        # return some usefull information
         return {
             "distance": np.linalg.norm(
                 self._agent_location - self._target_location, ord=1
