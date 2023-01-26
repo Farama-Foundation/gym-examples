2d96b98671e425c66f17e2017913299c704617b2
Zonghao Li <zonghao.lee@gmail.com>
2023-01-26 00:02:09 -0500

    now it poses requirement for gymnasium version as well

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="gym_examples",
     version="0.0.1",
-    install_requires=["gym==0.26.0", "pygame==2.1.0"],
+    install_requires=["gym>=0.26.0", "pygame>=2.1.0", "gymnasium>=0.27.1"],
 )
