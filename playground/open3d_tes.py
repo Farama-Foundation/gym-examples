2b4c222f7740596518e134882610555e43aef5ec
Gosudarkin Yaroslav <gosudarkin.yaroslav@gmail.com>
2023-02-10 19:41:15 +0300

    open3d poc

---
+++ playground/open3d_tes.py
@@ -0,0 +1,36 @@
+import numpy as np
+import open3d as o3d
+
+# proof of concept
+
+rng = np.random.default_rng()
+n_pcd = 10 * rng.random((10, 3)) - 10
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(n_pcd)
+o3d.visualization.draw_geometries([pcd])
+
+voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(input=pcd, voxel_size=0.1)
+
+octree = o3d.geometry.Octree(max_depth=5)
+octree.create_from_voxel_grid(voxel_grid)
+o3d.visualization.draw_geometries([octree])
+
+# api для "несуществующей координаты"
+
+voxel = np.asarray([40,40,412])
+print(f"FALSE OCTree coord: {octree.locate_leaf_node(voxel)}\n ______________ \n")
+
+# проверка потери/сохранения индексации
+
+print(f"PCloud coord: {pcd.points[0]}")
+print(f"Voxelgrid coord: {voxel_grid.get_voxel(pcd.points[0])}")
+print(f"OCTree coord: {octree.locate_leaf_node(voxel)}")
+
+print("____________________")
+
+octree_p = o3d.geometry.Octree(max_depth=5)
+octree_p.convert_from_point_cloud(point_cloud=pcd, size_expand=0.1)
+o3d.visualization.draw_geometries([octree_p])
+
+print(f"PCloud coord: {pcd.points[0]}")
+print(f"OCTree coord: {octree.locate_leaf_node(pcd.points[0])}")
